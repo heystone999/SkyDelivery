a4441d90f247643f7fa3482b1416e75dd19e3c56
Jie Zhao <j79zhao@uwaterloo.ca>
2023-11-25 15:28:07 -0500

    Swagger annotation

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -45,9 +45,9 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     @Bean
     public Docket docket() {
         ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("苍穹外卖项目接口文档")
+                .title("Sky Delivery API Documentation")
                 .version("2.0")
-                .description("苍穹外卖项目接口文档")
+                .description("Sky Delivery API Documentation")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo)
