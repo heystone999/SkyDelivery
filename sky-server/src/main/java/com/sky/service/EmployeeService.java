72a270cb1fb266dd3f6d9c4072cd7509f0985e07
Jie Zhao <j79zhao@uwaterloo.ca>
2023-11-26 12:11:59 -0500

    Enable/Disable (start/stop) employee account

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -31,4 +31,12 @@ public interface EmployeeService {
      * @return
      */
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 启用/禁用员工账号
+     *
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
