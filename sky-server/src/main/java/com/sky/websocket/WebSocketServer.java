d9cbc836bfb385777e63e2db2277016fb1371a2d
heystone999 <52831724+heystone999@users.noreply.github.com>
2023-12-23 20:16:43 -0500

    web socket

---
+++ sky-server/src/main/java/com/sky/websocket/WebSocketServer.java
@@ -0,0 +1,76 @@
+package com.sky.websocket;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Websocker服务
+ */
+@Component
+@ServerEndpoint("/ws/{sid}")
+public class WebSocketServer {
+    //存放会话对象
+    private static Map<String, Session> sessionMap = new HashMap();
+
+    /**
+     * 连接建立成功
+     *
+     * @param session
+     * @param sid
+     */
+    @OnOpen
+    public void onOpen(Session session, @PathParam("sid") String sid) {
+        System.out.println("客户端: " + sid + "建立连接");
+        sessionMap.put(sid, session);
+    }
+
+
+    /**
+     * 收到客户端消息
+     *
+     * @param message
+     * @param sid
+     */
+    @OnMessage
+    public void onMessage(String message, @PathParam("sid") String sid) {
+        System.out.println("收到来自客户端: " + sid + "的信息: " + message);
+    }
+
+    /**
+     * 连接关闭
+     *
+     * @param sid
+     */
+    @OnClose
+    public void onClose(@PathParam("sid") String sid) {
+        System.out.println("连接断开: " + sid);
+        sessionMap.remove(sid);
+    }
+
+    /**
+     * 群发
+     *
+     * @param message
+     */
+    public void sendToAllClient(String message) {
+        Collection<Session> sessions = sessionMap.values();
+        for (Session session : sessions) {
+            try {
+                //服务器向客户端发送消息
+                session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
