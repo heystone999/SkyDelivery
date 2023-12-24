490ec1d813577554e74f0ecac702119ed3b16125
heystone999 <52831724+heystone999@users.noreply.github.com>
2023-12-23 20:51:40 -0500

    fix: test new order reminder, skip WeChat payment callback, update status to TO_BE_CONFIRMED every five seconds, and send order notification to B

--- sky-server/src/main/java/com/sky/task/OrderTask.java
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -1,14 +1,18 @@
 package com.sky.task;
 
+import com.alibaba.fastjson.JSON;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 定时任务类 定时处理订单状态
@@ -18,6 +22,8 @@ import java.util.List;
 public class OrderTask {
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private WebSocketServer webSocketServer;
 
     /**
      * 处理超时订单
@@ -54,6 +60,30 @@ public class OrderTask {
             for (Orders orders : ordersList) {
                 orders.setStatus(Orders.COMPLETED);
                 orderMapper.update(orders);
+            }
+        }
+    }
+
+    /**
+     * 测试来单提醒 跳过微信支付回调 每五秒将PAID改为TO_BE_CONFIRMED 向商家推送订单消息 开启@Scheduled可测试
+     */
+//    @Scheduled(cron = "0/5 * * * * ?")
+    public void testPlaceSuccess() {
+        LocalDateTime time = LocalDateTime.now();
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PAID, time);
+        if (ordersList != null && ordersList.size() > 0) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.TO_BE_CONFIRMED);
+                orderMapper.update(orders);
+
+                //通过WebSocket向客户端浏览器推送消息
+                Map map = new HashMap();
+                map.put("type", 1); //1来单提醒 2客户催单
+                map.put("orderId", orders.getId());
+                map.put("content", "订单号: " + "qwertyuiop");
+
+                String json = JSON.toJSONString(map);
+                webSocketServer.sendToAllClient(json);
             }
         }
     }
